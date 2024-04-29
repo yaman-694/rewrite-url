cafbf8570be6c93559207873994aa80945a9cdc5
Yaman jain <72979343+yaman-694@users.noreply.github.com>
2024-04-29 10:03:17 +0530

    small change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // These rewrites are checked after headers/redirects
+        // and before all files including _next/public files which
+        // allows overriding page files
+        {
+          source: "/some-page",
+          destination: "/somewhere-else",
+          has: [{ type: "query", key: "overrideMe" }],
+        },
+      ],
+      afterFiles: [
+        // These rewrites are checked after pages/public files
+        // are checked but before dynamic routes
+        {
+          source: "/non-existent",
+          destination: "/somewhere-else",
+        },
+      ],
+      fallback: [
+        {
+          source: "/test:path*",
+          destination: `https://tutorials.recruitcrm.io/:path*`,
+        },
+      ],
+    };
+  },
+};
 
 export default nextConfig;
